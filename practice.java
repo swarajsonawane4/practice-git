010c824670b7d9d25902cfec10bf55719513a0d7
unknown <swaraj.sudhakar.sonawane@uni-weimar.de>
2024-05-28 16:16:16 +0530

    changes

--- practice.java
+++ practice.java
@@ -1,5 +1,8 @@
 public class practice {
     public static void main(String[] args) {
         System.out.println("swaraj sonawane");
+        System.out.println("mangala");
+        System.out.println("manoj");
+        System.out.println("siddhu");
     }
 }
